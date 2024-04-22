c1db2c393971fd7c06b5c5ddcf86f68b76eaf376
Satyam Gupta <satyam.gupta292002@gmail.com>
2024-04-22 16:56:50 +0530

    Add files via upload

---
+++ day3/hms/Patient.java
@@ -0,0 +1,23 @@
+package org.example.hms;
+
+
+public class Patient extends User {
+
+    private long patientId;
+    private boolean insured;
+
+    public long getPatientId() {
+        return patientId;
+    }
+    public void setPatientId(long patientId) {
+        this.patientId = patientId;
+    }
+
+    public boolean isInsured() {
+        return insured;
+    }
+    public void setInsured(boolean insured) {
+        this.insured = insured;
+    }
+}
+
