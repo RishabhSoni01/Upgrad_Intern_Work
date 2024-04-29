bb6abe2345e05e9d3caf7bc5129da0c168c03aa9
Satyam Gupta <satyam.gupta292002@gmail.com>
2024-04-29 18:27:37 +0530

    Update SpringOneApplication.java

--- day6/src/main/java/com/example/springOne/SpringOneApplication.java
+++ day6/src/main/java/com/example/springOne/SpringOneApplication.java
@@ -2,19 +2,23 @@ package com.example.springOne;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.ApplicationContext; 
 
 @SpringBootApplication
 public class SpringOneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringOneApplication.class, args);
-
-	    Vehicle mostExpensive=VehicleService.getMostExpensiveVehicle();
-		System.out.println("Most Expensive Vehicle Details:");
-		System.out.println("  Price: Rs" + mostExpensive.getPrice());
-		System.out.println("  Speaker Brand: " + mostExpensive.getSpeaker().getBrand());
-		System.out.println("  Tyre Brand: " + mostExpensive.getTyre().getBrand());
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(SpringOneApplication.class, args);
+        VehicleService vehicleService = context.getBean(VehicleService.class);
+        Vehicle mostExpensive = vehicleService.getMostExpensiveVehicle();
+        if (mostExpensive != null) {
+            System.out.println("Most Expensive Vehicle Details:");
+            System.out.println("  Price: Rs" + mostExpensive.getPrice());
+            System.out.println("  Speaker Brand: " + mostExpensive.getSpeaker().getBrand());
+            System.out.println("  Tyre Brand: " + mostExpensive.getTyre().getBrand());
+        } else {
+            System.out.println("No vehicles generated.");
+        }
+    }
 
 }
