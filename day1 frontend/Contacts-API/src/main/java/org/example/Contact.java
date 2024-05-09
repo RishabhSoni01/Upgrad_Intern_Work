b3ad367b708f35363e4a6c170a6b818a31c6f502
Satyam Gupta <satyam.gupta292002@gmail.com>
2024-05-10 00:20:16 +0530

    Add files via upload

---
+++ day1 frontend/Contacts-API/src/main/java/org/example/Contact.java
@@ -0,0 +1,78 @@
+package org.example;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private LocalDate dob;
+    private String imageUrl; // URL for the image
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
