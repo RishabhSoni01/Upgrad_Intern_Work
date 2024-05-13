0d70626a6f3eba06da2ee1c399bb03c507aaadc5
ashish6523 <araj6523@gmail.com>
2024-05-13 17:06:51 +0530

    Day8

---
+++ Day8_Ashish_UpGrad/demo/src/main/java/com/example/demo/Speaker.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+public class Speaker {
+
+    private String brand;
+    private double price;
+
+    public Speaker(String brand, double price) {
+        this.brand = brand;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
